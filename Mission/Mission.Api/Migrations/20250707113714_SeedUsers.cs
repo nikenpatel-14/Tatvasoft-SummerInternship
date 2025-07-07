1b67ba12a8882189052b730bdfea120511602745
Niken Patel <patelniken3333@gmail.com>
2025-07-07 17:40:40 +0530

    day4 of internship

---
+++ Mission/Mission.Api/Migrations/20250707113714_SeedUsers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "id", "CreatedDate", "email_address", "first_name", "IsDeleted", "last_name", "ModifiedDate", "password", "phone_number", "user_image", "user_type" },
+                values: new object[] { 2, new DateTime(2020, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc), "user@tatvasoft.com", "Niken", false, "Patel", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "User@123", "9876500000", "", "user" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 2);
+        }
+    }
+}
